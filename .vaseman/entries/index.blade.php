1b9ac60a102fed712540f3234227859864041933
Simon Asika <asika32764@gmail.com>
2022-10-11 01:41:58 +0800

    home wip

--- .vaseman/entries/index.blade.php
+++ .vaseman/entries/index.blade.php
@@ -85,29 +85,109 @@ class ArticleListView implements ViewModelInterface
                 </div>
             </div>
         </div>
+
+        <div class="c-banner-arrow">
+            <i class="fa-solid fa-angles-down fa-2x"></i>
+        </div>
     </x-hero-banner>
 
-    <div class="container">
-        <!-- Example row of columns -->
-        <div class="row my-4">
-            <div class="col-md-4">
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-secondary" href="{{ $uri->path() }}article/article.html"
-                        role="button">View details &raquo;</a></p>
+    <main class="">
+
+        <section class="container l-section l-section--features my-7">
+            <h2 class="l-section__title mb-6 fw-bold text-center">FEATURES</h2>
+
+            <!-- Feature Row -->
+            <div class="row">
+                <!-- Feature -->
+                <div class="feature-item col-md-4 text-center">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-face-grin-wide fa-4x" style="color: var(--bs-teal)"></i>
+                    </div>
+                    <h3 class="feature-title mt-4 mt-4">
+                        Easy but Powerful
+                    </h3>
+                    <p class="text-secondary">
+                        Learning a new framework is hard,
+                        we provide simple and semantic interface to help developers understand this framework.
+                    </p>
+                </div>
+                <!-- Feature -->
+                <div class="feature-item col-md-4 text-center">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-layer-group fa-4x" style="color: var(--bs-purple)"></i>
+                    </div>
+                    <h3 class="feature-title mt-4">
+                        Fully Decoupled
+                    </h3>
+                    <p class="text-secondary">
+                        Windwalker is a set of PHP tools, you can easily install them by composer without too many dependencies.
+                    </p>
+                </div>
+                <!-- Feature -->
+                <div class="feature-item col-md-4 text-center">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-expand fa-4x" style="color: var(--bs-orange)"></i>
+                    </div>
+                    <h3 class="feature-title mt-4">
+                        Extendable
+                    </h3>
+                    <p class="text-secondary">
+                        The package system helps us organize our classes and routing to build large enterprise level applications.
+                    </p>
+                </div>
             </div>
-            <div class="col-md-4">
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-secondary" href="{{ $uri->path() }}article/article.html"
-                        role="button">View details &raquo;</a></p>
+
+            <!-- Feature Row -->
+            <div class="row mt-5">
+                <!-- Feature -->
+                <div class="feature-item col-md-4 text-center">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-arrows-to-circle fa-4x" style="color: var(--bs-danger)"></i>
+                    </div>
+                    <h3 class="feature-title mt-4">
+                        Standard
+                    </h3>
+                    <p class="text-secondary">
+                        We follow PSR standard, you can easily integrate 3rd party middlewares or caching library into Windwalker.
+                    </p>
+                </div>
+                <!-- Feature -->
+                <div class="feature-item col-md-4 text-center">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-hat-wizard fa-4x" style="color: var(--bs-indigo)"></i>
+                    </div>
+                    <h3 class="feature-title mt-4">
+                        Rapid Development
+                    </h3>
+                    <p class="text-secondary">
+                        Windwalker is a RAD framework, building a usable system prototype with powerful UI is very fast.
+                    </p>
+                </div>
+                <!-- Feature -->
+                <div class="feature-item col-md-4 text-center">
+                    <div class="feature-icon">
+                        <i class="fa-solid fa-laptop-code fa-4x" style="color: var(--bs-info)"></i>
+                    </div>
+                    <h3 class="feature-title mt-4">
+                        IDE friendly
+                    </h3>
+                    <p class="text-secondary">
+                        Class searching, auto-completion and many useful IDE functions are working well with IoC.
+                    </p>
+                </div>
             </div>
-            <div class="col-md-4">
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <p><a class="btn btn-secondary" href="{{ $uri->path() }}article/article.html"
-                        role="button">View details &raquo;</a></p>
+
+        </section>
+
+        <section class="container l-section l-section--features my-7">
+            <h2 class="l-section__title mb-4 fw-bold text-center">GETTING STARTED</h2>
+
+            <div class="mt-4 row justify-content-center">
+                <div class="col-lg-6 col-md-8">
+                    <pre><code class="language-bash">$ composer create-project windwalker/starter</code></pre>
+                </div>
             </div>
-        </div>
-    </div> <!-- /container -->
+        </section>
+
+    </main> <!-- /container -->
 @stop
