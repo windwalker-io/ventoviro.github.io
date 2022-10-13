4c82c1847773f50f5fd299684c0f362a320b609e
Simon Asika <asika32764@gmail.com>
2022-10-13 23:33:10 +0800

    misc

---
+++ .vaseman/src/gen-index.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Part of site project.
+ *
+ * @copyright  Copyright (C) 2022 __ORGANIZATION__.
+ * @license    __LICENSE__
+ */
+
+declare(strict_types=1);
+
+$components = include __DIR__ . '/../resources/data/components.php';
+
+$docRoot = __DIR__ . '/../entries/documentation/components';
+
+foreach ($components as $component) {
+    $content = <<<MD
+---
+layout: global.documentation-layout
+title: Introduction
+part: Components
+chapter: 
+menu: components/$component
+---
+
+# Introduction
+
+This document is work in process.
+
+## Installation
+
+Install by composer
+
+```bash
+composer require windwalker/$component ^4.0
+```
+
+## Use in Windwalker
+
+...
+
+## Use as Standalone Component
+
+...
+
+## Getting Started
+
+...
+
+MD;
+    $file = $docRoot . '/index.md';
+
+    if (is_file($file)) {
+        file_put_contents($file, $content);
+    }
+
+}
